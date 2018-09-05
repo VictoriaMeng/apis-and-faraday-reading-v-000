e8c9aba0bfb35ba7191d5c4c2612e3d2e644870b
Victoria Meng <nights1ststar@protonmail.com>
2018-09-05 20:58:11 +0000

    automatically backed up by learn

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,9 +3,9 @@ class SearchesController < ApplicationController
   end
 
   def foursquare
-    Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
-      req.params['client_id'] = 52AJY54BZSRWZZ25T1STPBCMLF4AXGXV4RINHQPODIPWTEPB
-      req.params['client_secret'] = YNZIVP1XGE5T23ENOV2UQT4TDQW5N2HECVMRCBITXLQDJA25
+    @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
+      req.params['client_id'] = client_id
+      req.params['client_secret'] = client_secret
       req.params['v'] = '20160201'
       req.params['near'] = params[:zipcode]
       req.params['query'] = 'coffee shop'
